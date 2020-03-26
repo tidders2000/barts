8f1c92264886c31d8e7d9a64deb27f64fe787726
Simon Tidman <simontidman@Simons-MBP.lan>
2020-03-26 22:19:07 +0000

    added several hp sections

--- site1/static/js/jscript.js
+++ site1/static/js/jscript.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
-    $("wmodal").modal();
+
+    2
+    3
+    $(".btn-danger").click(function() {
+        $('#wmmodal').modal('show')
+    });
+
+
+
+
+    var observer = new IntersectionObserver(function(entries) {
+        // isIntersecting is true when element and viewport are overlapping
+        // isIntersecting is false when element and viewport don't overlap
+        if (entries[0].isIntersecting === true)
+            setTimeout(function() {
+                odometer.innerHTML = 456;
+                odometer2.innerHTML = 500;
+                odometer3.innerHTML = 989;
+            }, 1000);
+
+        AOS.init({
+            duration: 1200,
+        });
+    }, { threshold: [0] });
+
+    observer.observe(document.querySelector(".odometer"));
+
+
+
 
 });
---
+++ site1/static/js/maps.js
@@ -0,0 +1,161 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: {
+            lat: 52.912497,
+            lng: -0.643753
+
+        },
+        styles: [{
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#f5f5f5"
+                }]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#616161"
+                }]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#f5f5f5"
+                }]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#bdbdbd"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#eeeeee"
+                }]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#e5e5e5"
+                }]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#ffffff"
+                }]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#757575"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#dadada"
+                }]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#616161"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#e5e5e5"
+                }]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#eeeeee"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{
+                    "color": "#c9c9c9"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "color": "#9e9e9e"
+                }]
+            }
+        ]
+    });
+    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    var locations = [{
+        lat: 51.5080,
+        lng: -0.1281
+    }, {
+        lat: 52.765237,
+        lng: -0.886775
+    }, {
+        lat: 53.072759,
+        lng: -0.813535
+    }, {
+        lat: 52.930876,
+        lng: -1.133776
+    }, {
+        lat: 52.765237,
+        lng: -0.380889
+    }];
+
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
