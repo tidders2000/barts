1778ccd949b0ef18dc4ad372aedb3da63c846182
Simon Tidman <simontidman@Simons-MBP.lan>
2020-05-04 22:08:17 +0100

    site 1 content added

--- site1/static/js/jscript.js
+++ site1/static/js/jscript.js
@@ -13,15 +13,17 @@ $(document).ready(function() {
         // isIntersecting is false when element and viewport don't overlap
         if (entries[0].isIntersecting === true)
             setTimeout(function() {
-                odometer.innerHTML = 456;
-                odometer2.innerHTML = 500;
+                odometer.innerHTML = 290;
+                odometer2.innerHTML = 100;
                 odometer3.innerHTML = 989;
             }, 1000);
 
         AOS.init({
             duration: 1200,
         });
-    }, { threshold: [0] });
+    }, {
+        threshold: [0]
+    });
 
     observer.observe(document.querySelector(".odometer"));
 
